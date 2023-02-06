0d270c5343b3d886ea47b8688a3d59a96cc5c3a4
Kanai <31379945+Kanai2015@users.noreply.github.com>
2023-02-07 00:21:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'Hello Ditipriya - Here you go with your first GitOps demo\... TY!!!'
